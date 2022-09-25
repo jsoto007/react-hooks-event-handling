7b325e384fd0cebd31d615ce0b97d5b31e60602a
Jose Soto <jsoto7087@gmail.com>
2022-09-25 12:16:54 -0400

    commit tree 05638ca6c5ea7fcff7f335014a49525483d80e54

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tickler() {
   function tickle(event) {
-    console.log(event);
+    console.log(event.target.innerText);
   }
 
   return <button onClick={tickle}>Tickle me!</button>;
